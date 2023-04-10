998874752341718c874ae06309261bf860c25aac
Siddhartha Basu <biosidd@gmail.com>
2023-04-10 13:19:10 -0500

    feat(instance.ts): add variables for instance name, machine type, and boot disk size and create ComputeInstance resource and output its IP address
    The instance.ts file now has variables for instance name, machine type,
    and boot disk size. A ComputeInstance resource is created with the
    specified variables and added to the Terraform stack. The IP address of
    the instance is outputted as a Terraform output. This change allows for
    more flexibility in creating instances with different configurations and
    outputs the IP address for use in other resources.

--- instance.ts
+++ instance.ts
@@ -1,10 +1,12 @@
 import * as cdktf from "cdktf"
 import { Construct } from "constructs"
-import { TerraformStack } from "cdktf"
+import { TerraformStack, TerraformOutput } from "cdktf"
 import * as path from "path"
 import * as fs from "fs"
 import { GoogleProvider } from "@cdktf/provider-google/lib/provider"
 import VpcNetwork from "./vpc"
+import { K8Disk } from "./disk"
+import { ComputeInstance } from "@cdktf/provider-google/lib/compute-instance"
 
 class VmInstanceStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -32,15 +34,46 @@ class VmInstanceStack extends TerraformStack {
       description:
         "The IP range in cidr notation for vpc subnet that will be assigned to nodes",
     })
+    const instanceName = new cdktf.TerraformVariable(this, "instance", {
+      type: "string",
+      default: "k8-master",
+      description: "name of the instance",
+    })
+    const machineType = new cdktf.TerraformVariable(this, "machine-tyoe", {
+      type: "string",
+      default: "custom-2-2048",
+      description: "The machine type to create",
+    })
+    const bootDiskSize = new cdktf.TerraformVariable(this, "disk-size", {
+      type: "number",
+      default: 30,
+      description: "size of the boot disk in GB",
+    })
     new GoogleProvider(this, "google", {
       credentials: this._read_credentials("credentials.json"),
       project: projectId.value,
       region: region.value,
       zone: zone.value,
     })
-    new VpcNetwork(this, id, {
+    const vpcNetwork = new VpcNetwork(this, id, {
       ports: ports.value,
       ipCidrRange: ipCidrRange.value,
+    })
+    const k8Disk = new K8Disk(this, id, { size: bootDiskSize.value })
+    const instance = new ComputeInstance(this, "instance", {
+      name: instanceName.value,
+      machineType: machineType.value,
+      bootDisk: {
+        source: k8Disk.disk.id,
+      },
+      networkInterface: [{ network: vpcNetwork.network.id }],
+      allowStoppingForUpdate: true,
+      scheduling: {
+        provisioningModel: "STANDARD",
+      },
+    })
+    new TerraformOutput(this, "ip-addr", {
+      value: instance.networkInterface.get(0).accessConfig.get(0).natIp,
     })
   }
 
