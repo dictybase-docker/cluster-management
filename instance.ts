31eeab959a53309d66113a928836fec966417e20
Siddhartha Basu <biosidd@gmail.com>
2023-04-13 20:40:02 -0500

    refactor(instance.ts): remove unused name property from VmInstanceProperties
    The name property is not used in the VmInstanceProperties interface, so
    it has been removed. The instance name is now set to the id parameter
    passed to the VmInstance constructor, which improves consistency with
    the naming conventions.

--- instance.ts
+++ instance.ts
@@ -5,7 +5,6 @@ import { ComputeNetwork } from "@cdktf/provider-google/lib/compute-network"
 import { ComputeInstance } from "@cdktf/provider-google/lib/compute-instance"
 
 type VmInstanceProperties = {
-  name: TerraformVariable
   machine: TerraformVariable
   disk: ComputeDisk
   network: ComputeNetwork
@@ -14,10 +13,10 @@ type VmInstanceProperties = {
 class VmInstance extends Construct {
   public readonly vmInstance: ComputeInstance
   constructor(scope: Construct, id: string, properties: VmInstanceProperties) {
-    super(scope, `${id}-vminstance`)
-    const { name, machine, disk, network } = properties
-    this.vmInstance = new ComputeInstance(this, "instance", {
-      name: name.value,
+    super(scope, id)
+    const { machine, disk, network } = properties
+    this.vmInstance = new ComputeInstance(this, id, {
+      name: id,
       machineType: machine.value,
       bootDisk: {
         source: disk.id,
