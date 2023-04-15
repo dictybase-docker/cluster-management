4a00119562f94e36afc61d1358ccde4e94bfd358
Siddhartha Basu <biosidd@gmail.com>
2023-04-15 17:35:30 -0500

    feat(main.ts): add yargs for command line argument parsing
    The yargs package is added to the project to parse command line
    arguments. Two options are added, `remote` and `nodes`. The `remote`
    option is a boolean that specifies whether the remote gcs backend will
    be used. The `nodes` option is a number that specifies the number of VM
    instances for kubernetes nodes.

--- main.ts
+++ main.ts
@@ -1,5 +1,23 @@
 import { App } from "cdktf"
 import { K8Stack } from "./k8s"
+import yargs from "yargs/yargs"
+
+const argv = yargs(process.argv.slice(2))
+  .options({
+    r: {
+      alias: "remote",
+      type: "boolean",
+      default: true,
+      description: "whether the remote gcs backend will be used",
+    },
+    n: {
+      alias: "nodes",
+      description: "number of VM instances for kubernetes nodes",
+      type: "number",
+      default: 3,
+    },
+  })
+  .parseSync()
 
 const app = new App()
 "REMOTE" in process.env
