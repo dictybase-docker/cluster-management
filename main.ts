a56ad7258949ca20740ae96055253f30d64492b5
Siddhartha Basu <biosidd@gmail.com>
2023-04-17 16:40:00 -0500

    feat(main.ts): add support for service account credentials file for google provider
    The new `--credentials` option allows users to specify the service
    account credentials file for the google provider. This is useful when
    running the application in a cloud environment where the credentials are
    stored in a file. The `K8Stack` constructor now accepts a `credentials`
    parameter which is passed to the `GoogleProvider` constructor.

--- main.ts
+++ main.ts
@@ -16,9 +16,19 @@ const argv = yargs(process.argv.slice(2))
       type: "number",
       default: 3,
     },
+    c: {
+      alias: "credentials",
+      description: "service account credentials file for google provider",
+      type: "string",
+      default: "credentials.json",
+    },
   })
   .parseSync()
 
 const app = new App()
-new K8Stack(app, "k0s-cluster-cdktf", { remote: argv.r, nodes: argv.n })
+new K8Stack(app, "k0s-cluster-cdktf", {
+  remote: argv.r,
+  nodes: argv.n,
+  credentials: argv.c,
+})
 app.synth()
