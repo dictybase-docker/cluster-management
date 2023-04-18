a6e08ad3b335caa1ab0142dc4f341e45abed8e7f
Siddhartha Basu <biosidd@gmail.com>
2023-04-18 13:59:22 -0500

    refactor(main.ts): move command line options to separate file
    The command line options have been moved to a separate file named
    command_options.ts.

--- main.ts
+++ main.ts
@@ -1,42 +1,6 @@
 import { App } from "cdktf"
 import { K8Stack } from "./k8s"
-import yargs from "yargs/yargs"
-
-const argv = yargs(process.argv.slice(2))
-  .options({
-    bn: {
-      alias: "bucket-name",
-      type: "string",
-      default: "dicty-terraform-state",
-      description: "GCS bucket name where terraform remote state is stored.",
-    },
-    bp: {
-      alias: "bucket-prefix",
-      type: "string",
-      default: "k0s-cluster-cdktf",
-      description:
-        "GCS bucket folder prefix where terraform remote state is stored.",
-    },
-    r: {
-      alias: "remote",
-      type: "boolean",
-      default: true,
-      description: "whether the remote gcs backend will be used",
-    },
-    n: {
-      alias: "nodes",
-      description: "number of VM instances for kubernetes nodes",
-      type: "number",
-      default: 2,
-    },
-    c: {
-      alias: "credentials",
-      description: "service account credentials file for google provider",
-      type: "string",
-      default: "credentials.json",
-    },
-  })
-  .parseSync()
+import { argv } from "./command_options"
 
 const app = new App()
 new K8Stack(app, "k0s-cluster-cdktf", {
