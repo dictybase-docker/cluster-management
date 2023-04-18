f38c87770ddf226f737bbc864ec017f4e52e538b
Siddhartha Basu <biosidd@gmail.com>
2023-04-18 13:33:46 -0500

    feat(instance.ts): add support for subnetwork in VmInstanceProperties
    The VmInstanceProperties now include a subnetwork property, which is
    used to specify the subnetwork for the instance's network interface.

--- instance.ts
+++ instance.ts
@@ -2,26 +2,28 @@ import { Construct } from "constructs"
 import { TerraformVariable } from "cdktf"
 import { ComputeDisk } from "@cdktf/provider-google/lib/compute-disk"
 import { ComputeNetwork } from "@cdktf/provider-google/lib/compute-network"
+import { ComputeSubnetwork } from "@cdktf/provider-google/lib/compute-subnetwork"
 import { ComputeInstance } from "@cdktf/provider-google/lib/compute-instance"
 
 type VmInstanceProperties = {
   machine: TerraformVariable
   disk: ComputeDisk
   network: ComputeNetwork
+  subnetwork: ComputeSubnetwork
 }
 
 class VmInstance extends Construct {
   public readonly vmInstance: ComputeInstance
   constructor(scope: Construct, id: string, properties: VmInstanceProperties) {
     super(scope, id)
-    const { machine, disk, network } = properties
+    const { machine, disk, network, subnetwork } = properties
     this.vmInstance = new ComputeInstance(this, id, {
       name: id,
       machineType: machine.value,
       bootDisk: {
         source: disk.id,
       },
-      networkInterface: [{ network: network.id }],
+      networkInterface: [{ network: network.id, subnetwork: subnetwork.id }],
       allowStoppingForUpdate: true,
       scheduling: {
         provisioningModel: "STANDARD",
