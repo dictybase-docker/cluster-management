a9455d085b88899b72e2291b8ec7d9e9b3a007a8
Siddhartha Basu <biosidd@gmail.com>
2023-04-19 13:49:10 -0500

    feat(stack_utils.ts): add numberToText and times functions
    The numberToText function converts a number to its textual
    representation. The times function is a recursive function that calls a
    callback function n times.

---
+++ stack_utils.ts
@@ -0,0 +1,54 @@
+const numberToText = (num: number): string => {
+  const ones = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+  ]
+  const tens = [
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+  ]
+  switch (true) {
+    case num === 0:
+      return "zero"
+    case num > 0 && num < 20:
+      return ones[num - 1]
+    case num > 20 && num < 100:
+      return tens[Math.floor(num / 10)].concat(
+        num % 10 !== 0 ? "" : ones[(num % 10) - 1],
+      )
+    default:
+      break
+  }
+  return "none"
+}
+
+const times = (n: number, callback: () => void) => {
+  if (n <= 0) return
+  callback()
+  times(n - 1, callback)
+}
+
+export { times, numberToText }
