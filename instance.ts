b73c162d2d08f7c46589a4220e4168d33fea4db2
Siddhartha Basu <biosidd@gmail.com>
2023-04-18 20:15:45 -0500

    feat(instance.ts): add support for static IP address
    The VmInstance class now has a staticIPaddress property that creates a
    new ComputeAddress instance. The static IP address is then used in the
    networkInterface property of the ComputeInstance instance to assign a
    static IP address to the instance.

--- instance.ts
+++ instance.ts
@@ -4,6 +4,7 @@ import { ComputeDisk } from "@cdktf/provider-google/lib/compute-disk"
 import { ComputeNetwork } from "@cdktf/provider-google/lib/compute-network"
 import { ComputeSubnetwork } from "@cdktf/provider-google/lib/compute-subnetwork"
 import { ComputeInstance } from "@cdktf/provider-google/lib/compute-instance"
+import { ComputeAddress } from "@cdktf/provider-google/lib/compute-address"
 
 type VmInstanceProperties = {
   machine: TerraformVariable
@@ -14,16 +15,26 @@ type VmInstanceProperties = {
 
 class VmInstance extends Construct {
   public readonly vmInstance: ComputeInstance
+  public readonly staticIPaddress: ComputeAddress
   constructor(scope: Construct, id: string, properties: VmInstanceProperties) {
     super(scope, id)
     const { machine, disk, network, subnetwork } = properties
+    this.staticIPaddress = new ComputeAddress(this, `${id}-static-ip-address`, {
+      name: `${id}-static-ip-address`,
+    })
     this.vmInstance = new ComputeInstance(this, id, {
       name: id,
       machineType: machine.value,
       bootDisk: {
         source: disk.id,
       },
-      networkInterface: [{ network: network.id, subnetwork: subnetwork.id }],
+      networkInterface: [
+        {
+          network: network.id,
+          subnetwork: subnetwork.id,
+          accessConfig: [{ natIp: this.staticIPaddress.address }],
+        },
+      ],
       allowStoppingForUpdate: true,
       scheduling: {
         provisioningModel: "STANDARD",
