943de1606eafad10c97648aa9a8909b3bb8def6f
Siddhartha Basu <biosidd@gmail.com>
2023-04-19 08:11:17 -0500

    feat(main.ts): add K0Stack to the application
    The K0Stack is added to the application, which allows for the creation
    of a k0s cluster. This is in addition to the existing K8Stack that
    creates a kubernetes cluster. The K0Stack is created with the same
    arguments as the K8Stack.

--- main.ts
+++ main.ts
@@ -1,13 +1,22 @@
 import { App } from "cdktf"
 import { K8Stack } from "./k8stack"
+import { K0Stack } from "./k0stack"
 import { argv } from "./command_options"
 
 const app = new App()
-new K8Stack(app, "k0s-cluster-cdktf", {
+new K8Stack(app, "k0s-vm-cdktf", {
   remote: argv.r,
   nodes: argv.n,
   credentials: argv.c,
   bucketName: argv.bn,
   bucketPrefix: argv.bp,
 })
+new K0Stack(app, "k0s-cluster-cdktf", {
+  remote: argv.r,
+  nodes: argv.n,
+  credentials: argv.c,
+  bucketName: argv.bn,
+  bucketPrefix: argv.bp,
+})
+
 app.synth()
