dde68e7e12dd657c4365ec42b2e70c9b3c2f5603
Siddhartha Basu <biosidd@gmail.com>
2023-04-24 13:37:11 -0500

    refactor(k8stack.ts): remove unused TerraformVariable definitions and define options object destructuring
    The TerraformVariable definitions for ports, ipCidrRange,
    masterInstanceName, masterMachineType, masterDiskSize, nodeInstanceName,
    nodeMachineType, and nodeDiskSize are removed as they are not used in
    the code.

--- k8stack.ts
+++ k8stack.ts
@@ -1,4 +1,4 @@
-import { TerraformStack, TerraformVariable, GcsBackend } from "cdktf"
+import { TerraformStack, GcsBackend } from "cdktf"
 import { Construct } from "constructs"
 import * as fs from "fs"
 import { GoogleProvider } from "@cdktf/provider-google/lib/provider"
@@ -33,122 +33,67 @@ class K8Stack extends TerraformStack {
   public readonly workers: Array<ComputeInstance>
   constructor(scope: Construct, id: string, options: K8StackProperties) {
     super(scope, id)
-    const { project, region, zone } = options
-    const variables = this.#define_variables()
-    if (options.remote) {
+    const {
+      remote,
+      nodes,
+      credentials,
+      bucketName,
+      bucketPrefix,
+      project,
+      region,
+      zone,
+      ports,
+      ipCidrRange,
+      masterInstanceName,
+      masterMachineType,
+      masterDiskSize,
+      nodeInstanceName,
+      nodeMachineType,
+      nodeDiskSize,
+      sshKeyFile,
+    } = options
+    if (remote) {
       new GcsBackend(this, {
-        bucket: options.bucketName,
-        prefix: options.bucketPrefix,
-        credentials: fs.readFileSync(options.credentials).toString(),
+        bucket: bucketName,
+        prefix: bucketPrefix,
+        credentials: fs.readFileSync(credentials).toString(),
       })
     }
     new GoogleProvider(this, "google", {
-      credentials: fs.readFileSync(options.credentials).toString(),
+      credentials: fs.readFileSync(credentials).toString(),
       project: project,
       region: region,
       zone: zone,
     })
     const vpcNetwork = new VpcNetwork(this, `${id}-vpc`, {
-      ports: variables.get("ports").value,
-      ipCidrRange: variables.get("ipCidrRange").value,
+      ports: ports,
+      ipCidrRange: ipCidrRange,
     })
-    const sshKey = this.#read_key_file(options.sshKeyFile)
+    const sshKey = this.#read_key_file(sshKeyFile)
     this.master = new VmInstance(this, `${id}-vm-master`, {
-      machine: variables.get("masterMachineType"),
+      machine: masterMachineType,
       disk: new K8Disk(this, `${id}-disk-master`, {
-        size: variables.get("masterDiskSize").value,
+        size: masterDiskSize,
       }).disk,
       network: vpcNetwork.network,
       subnetwork: vpcNetwork.subnetwork,
       sshKey: sshKey,
     }).vmInstance
-    this.workers = [...Array(options.nodes + 1).keys()]
+    this.workers = [...Array(nodes + 1).keys()]
       .filter((num) => num !== 0)
       .map(
         (num) =>
           new VmInstance(this, `${id}-vm-node-${numberToText(num)}`, {
-            machine: variables.get("nodeMachineType"),
+            machine: nodeMachineType,
             network: vpcNetwork.network,
             subnetwork: vpcNetwork.subnetwork,
             sshKey: sshKey,
             disk: new K8Disk(this, `${id}-disk-node-${numberToText(num)}`, {
-              size: variables.get("nodeDiskSize").value,
+              size: nodeDiskSize,
             }).disk,
           }),
       )
       .map((w) => w.vmInstance)
-  }
-
-  #define_variables() {
-    const variables = new Map()
-    variables
-      .set(
-        "ports",
-        new TerraformVariable(this, "ports", {
-          description: "ports that will be opened in the network",
-          type: "list(string)",
-          default: ["8955"],
-        }),
-      )
-      .set(
-        "ipCidrRange",
-        new TerraformVariable(this, "ip-cidr-range", {
-          default: "10.8.0.0/21",
-          description:
-            "The IP range in cidr notation for vpc subnet that will be assigned to nodes",
-        }),
-      )
-      .set(
-        "masterInstanceName",
-        new TerraformVariable(this, "master_instance", {
-          type: "string",
-          default: "k8-master",
-          description:
-            "name of the instance that will be used for kubernetes controller",
-        }),
-      )
-      .set(
-        "masterMachineType",
-        new TerraformVariable(this, "master-machine-type", {
-          type: "string",
-          default: "custom-2-2048",
-          description: "The machine type for kubernetes controller",
-        }),
-      )
-      .set(
-        "masterDiskSize",
-        new TerraformVariable(this, "master-disk-size", {
-          type: "number",
-          default: 30,
-          description: "size of the boot disk of kubernetes master in GB",
-        }),
-      )
-      .set(
-        "nodeInstanceName",
-        new TerraformVariable(this, "node_instance", {
-          type: "string",
-          default: "k8-node",
-          description:
-            "name of the instance that will be used for kubernetes nodes",
-        }),
-      )
-      .set(
-        "nodeMachineType",
-        new TerraformVariable(this, "node-machine-type", {
-          type: "string",
-          default: "custom-2-4096",
-          description: "The machine type for kubernetes node",
-        }),
-      )
-      .set(
-        "nodeDiskSize",
-        new TerraformVariable(this, "node-disk-size", {
-          type: "number",
-          default: 50,
-          description: "size of the boot disk of kubernetes nodes in GB",
-        }),
-      )
-    return variables
   }
 
   #read_key_file(file: string) {
