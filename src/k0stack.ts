89da67c2bdd4653ec1764cc56ce1039bbe7dbac9
Siddhartha Basu <biosidd@gmail.com>
2023-04-28 11:58:21 -0500

    refactor(k0stack.ts): remove unused TerraformVariable import and #define_variables method
    The TerraformVariable import is no longer used in the K0Stack class, so
    it has been removed. The #define_variables method is also no longer
    used, so it has been removed as well.

--- src/k0stack.ts
+++ src/k0stack.ts
@@ -1,4 +1,4 @@
-import { TerraformStack, TerraformVariable, TerraformOutput } from "cdktf"
+import { TerraformStack, TerraformOutput } from "cdktf"
 import { Construct } from "constructs"
 import { ComputeInstance } from "@cdktf/provider-google/lib/compute-instance"
 import { numberToText } from "./stack_utils"
@@ -12,7 +12,6 @@ class K0Stack extends TerraformStack {
   constructor(scope: Construct, id: string, options: K0StackProperties) {
     super(scope, id)
     const { master, workers } = options
-    this.#define_variables()
     new TerraformOutput(this, "master-node-ip", {
       value: master.networkInterface.get(0).accessConfig.get(0).natIp,
     })
@@ -21,33 +20,6 @@ class K0Stack extends TerraformStack {
         value: w.networkInterface.get(0).accessConfig.get(0).natIp,
       })
     })
-  }
-  #define_variables() {
-    const variables = new Map()
-    variables
-      .set(
-        "projectId",
-        new TerraformVariable(this, "project_id", {
-          description: "gcp project id",
-          type: "string",
-          nullable: false,
-        }),
-      )
-      .set(
-        "region",
-        new TerraformVariable(this, "region", {
-          default: "us-central1",
-          description: "gcp region",
-        }),
-      )
-      .set(
-        "zone",
-        new TerraformVariable(this, "zone", {
-          default: "us-central1-c",
-          description: "gcp zone name within a region",
-        }),
-      )
-    return variables
   }
 }
 
