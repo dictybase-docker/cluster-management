a8ed52e0674e069f5526ebaa54d0203545a184a8
Siddhartha Basu <biosidd@gmail.com>
2023-04-28 13:15:22 -0500

    feat(k0stack.ts): add support for generating k0s cluster.yml file
    The K0Stack class now accepts additional properties such as version,
    user, and output. The createClusterYml function is called to generate
    the cluster.yml file based on the provided properties. The generated
    file is then written to the specified output file path.

--- src/k0stack.ts
+++ src/k0stack.ts
@@ -2,17 +2,31 @@ import { TerraformStack, TerraformOutput } from "cdktf"
 import { Construct } from "constructs"
 import { ComputeInstance } from "@cdktf/provider-google/lib/compute-instance"
 import { numberToText } from "./stack_utils"
+import { HostNodeProperties } from "./k0s_cluster"
+import { createClusterYml } from "./k0s_cluster"
+import { writeFileSync } from "fs"
 
 type K0StackProperties = {
   master: ComputeInstance
   workers: Array<ComputeInstance>
   sshKeyFile: string
+  version: string
+  user: string
+  output: string
 }
 
 class K0Stack extends TerraformStack {
   constructor(scope: Construct, id: string, options: K0StackProperties) {
     super(scope, id)
-    const { master, workers } = options
+    const { master, workers, sshKeyFile, version, user, output } = options
+    const hosts: Array<HostNodeProperties> = [
+      {
+        user: user,
+        keyPath: sshKeyFile,
+        role: "controller",
+        address: master.networkInterface.get(0).accessConfig.get(0).natIp,
+      },
+    ]
     new TerraformOutput(this, "master-node-ip", {
       value: master.networkInterface.get(0).accessConfig.get(0).natIp,
     })
@@ -20,7 +34,18 @@ class K0Stack extends TerraformStack {
       new TerraformOutput(this, `workder-node-${numberToText(idx + 1)}-ip`, {
         value: w.networkInterface.get(0).accessConfig.get(0).natIp,
       })
+      hosts.push({
+        user: user,
+        keyPath: sshKeyFile,
+        role: "worker",
+        address: w.networkInterface.get(0).accessConfig.get(0).natIp,
+      })
     })
+    const clusterYml = createClusterYml({
+      version: version,
+      hosts,
+    })
+    writeFileSync(output, clusterYml)
   }
 }
 
