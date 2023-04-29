98e632e26a0722e2e5cf90604f99ab512fc60a8c
Siddhartha Basu <biosidd@gmail.com>
2023-04-29 13:12:40 -0500

    feat(generate_cluster_config.ts): add script to generate k0s cluster configuration
    This commit adds a new script that generates a k0s cluster configuration
    file based on the output of a Terraform run. The script takes in several
    command-line arguments, including the path to the Terraform output file,
    the path to the public SSH key file, the Kubernetes version, and the
    output file path. The script reads the Terraform output file, extracts
    the public IP addresses of the VM instances, and generates a YAML file
    that contains the necessary configuration for the k0s cluster.

---
+++ src/generate_cluster_config.ts
@@ -0,0 +1,69 @@
+import yargs from "yargs/yargs"
+import { writeFileSync, readFileSync } from "fs"
+import { createClusterYml } from "./k0s_cluster"
+
+type TerraformOutputProperties = {
+  instance: any
+  cluster: {
+    master: string
+    workers: Array<string>
+  }
+}
+
+const argv = yargs(process.argv.slice(2))
+  .options({
+    sk: {
+      alias: "ssh-key",
+      description:
+        "public ssh key file that will be injected in the vm for login",
+      type: "string",
+      default: "k8sVM",
+    },
+    kv: {
+      alias: "k8-version",
+      type: "string",
+      demandOption: true,
+      description: "kubernetes cluster version",
+    },
+    u: {
+      alias: "user",
+      type: "string",
+      default: "newman",
+      description: "ssh login user",
+    },
+    o: {
+      alias: "output",
+      type: "string",
+      default: "cluster_config.yml",
+      description: "output yml file for writing the cluster configuration",
+    },
+    i: {
+      alias: "input",
+      type: "string",
+      default: "ip.json",
+      description:
+        "json output file generated from terraform run containing the public ips of vm instance",
+    },
+  })
+  .parseSync()
+
+const terraformObject: TerraformOutputProperties = JSON.parse(
+  readFileSync(argv.i).toString(),
+)
+const hosts = [
+  Object.create({
+    role: "controller",
+    user: argv.u,
+    keyPath: argv.sk,
+    address: terraformObject.cluster.master,
+  }),
+  ...terraformObject.cluster.workers.map((ip) =>
+    Object.create({
+      role: "worker",
+      user: argv.u,
+      keyPath: argv.sk,
+      address: ip,
+    }),
+  ),
+]
+writeFileSync(argv.o, createClusterYml({ version: argv.kv, hosts }))
