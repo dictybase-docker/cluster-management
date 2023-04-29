b91bf80d307bc63e018595090293ab246f75d05c
Siddhartha Basu <biosidd@gmail.com>
2023-04-29 08:02:07 -0500

    refactor(k0stack.ts): rename TerraformOutput names and remove unused import
    The TerraformOutput names have been renamed to better reflect their
    values. The `numberToText` function is no longer used and has been
    removed.

--- src/k0stack.ts
+++ src/k0stack.ts
@@ -1,7 +1,6 @@
 import { TerraformStack, TerraformOutput } from "cdktf"
 import { Construct } from "constructs"
 import { ComputeInstance } from "@cdktf/provider-google/lib/compute-instance"
-import { numberToText } from "./stack_utils"
 
 type K0StackProperties = {
   master: ComputeInstance
@@ -12,13 +11,13 @@ class K0Stack extends TerraformStack {
   constructor(scope: Construct, id: string, options: K0StackProperties) {
     super(scope, id)
     const { master, workers } = options
-    new TerraformOutput(this, "master-node-ip", {
+    new TerraformOutput(this, "master", {
       value: master.networkInterface.get(0).accessConfig.get(0).natIp,
     })
-    workers.forEach((w, idx) => {
-      new TerraformOutput(this, `workder-node-${numberToText(idx + 1)}-ip`, {
-        value: w.networkInterface.get(0).accessConfig.get(0).natIp,
-      })
+    new TerraformOutput(this, "workers", {
+      value: workers.map(
+        (w) => w.networkInterface.get(0).accessConfig.get(0).natIp,
+      ),
     })
   }
 }
