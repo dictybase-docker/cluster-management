895b292c0ed80dca65fb2b8f9eafee4232a24271
Siddhartha Basu <biosidd@gmail.com>
2023-05-15 14:03:48 -0500

    refactor(bootstrap_microk8s.sh): move script to scripts directory and simplify it
    The script has been moved to the scripts directory to keep the project
    organized. The script has also been simplified to only install microk8s
    using snap.

--- bootstrap_microk8s.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-dbus-uuidgen > /var/lib/dbus/machine-id
-snap install microk8s --classic --channel=1.26/stable
-usermod -a -G microk8s $USER
-microk8s status --wait-ready
-microk8s.config  > ${HOME}/kubeconfig
-microk8s enable dns
---
+++ scripts/bootstrap_microk8s.sh
@@ -0,0 +1,2 @@
+!#/bin/sh
+sudo snap install microk8s --classic --channel=1.26/stable
