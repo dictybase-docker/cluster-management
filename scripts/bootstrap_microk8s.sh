aaf895412ed54d2c27b4e21d6e850968043c8038
Siddhartha Basu <biosidd@gmail.com>
2023-05-18 07:22:41 -0500

    refactor(bootstrap_microk8s.sh): simplify if statement and add wait for microk8s to be ready before continuing
    The if statement has been simplified to make it more readable. A new
    feature has been added to wait for microk8s to be ready before
    continuing with the script. This ensures that the script does not fail
    due to microk8s not being ready.

--- scripts/bootstrap_microk8s.sh
+++ scripts/bootstrap_microk8s.sh
@@ -1,8 +1,7 @@
-if ! [ -f `which microk8s` ];
-then
+if ! `which microk8s` ; then
     sudo snap install microk8s --classic --channel=1.26/stable
+    sudo microk8s status --wait-ready
 fi
 sudo usermod -a -G microk8s $USER
-sudo microk8s status --wait-ready
 sudo microk8s enable dns
 sudo microk8s.config > kubeconfig.yaml
