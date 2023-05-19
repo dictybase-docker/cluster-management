4f43f5241bef443685a05a0010ae9d2b4fe191d0
Siddhartha Basu <biosidd@gmail.com>
2023-05-19 07:53:08 -0500

    feat(gce.sh): add script to set environment variables for GCE deployment
    This commit adds a new script `gce.sh` which sets environment variables
    for deploying the application on Google Compute Engine (GCE). The script
    sets variables for the cluster name, GCP project, GCP network name,
    image ID, GCP region, control plane machine type, node machine type, and
    Kubernetes version. These variables can be used to deploy the
    application on GCE.

---
+++ scripts/gce.sh
@@ -0,0 +1,9 @@
+# export CLUSTER_NAME="<CLUSTER_NAME>"
+# export GCP_PROJECT=solid-topic-90123
+# export GCP_NETWORK_NAME=<GCP_NETWORK_NAME or default>
+# export IMAGE_ID=projects/$GCP_PROJECT/global/images/<built image>
+
+export GCP_REGION=us-central1
+export GCP_CONTROL_PLANE_MACHINE_TYPE=custom-2-2048
+export GCP_NODE_MACHINE_TYPE=custom-2-4096
+export KUBERNETES_VERSION=1.25.8
