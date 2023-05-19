8ab20bcb09d25e9f9ccde2d0938fd3e95052db03
Siddhartha Basu <biosidd@gmail.com>
2023-05-19 17:39:57 -0500

    feat(bootstrap_microk8s.sh): add Google Cloud Operations Agent installation
    This commit adds the installation of the Google Cloud Operations Agent
    to the bootstrap_microk8s.sh script. The agent is installed by
    downloading the installation script from Google Cloud's website and
    running it with the --also-install flag.

--- scripts/bootstrap_microk8s.sh
+++ scripts/bootstrap_microk8s.sh
@@ -1,3 +1,5 @@
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 if ! `which microk8s` ; then
     sudo snap install microk8s --classic --channel=1.24-eksd/stable
     sudo microk8s status --wait-ready
