d36eae2cfab80368b5430770ffb5a1e5f8dafaa7
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-05-24 20:59:38 -0500

    feat(create_cluster.ts): add functionality to create a kops cluster with specified parameters
    This commit adds a new file `create_cluster.ts` which contains a
    function `runKops` that creates a kops cluster with the specified
    parameters. The function takes in command line arguments and constructs
    a kops command to create a cluster with the specified parameters. The
    `runCommand` function is used to execute the kops command. The function
    logs debug and error messages using the `winston` logger.

---
+++ src/kops/create_cluster.ts
@@ -0,0 +1,55 @@
+import { Logger as LogHandler } from "winston"
+import { exec } from "child_process"
+import { getLogger } from "./log"
+import { argv } from "./command_line"
+
+type runCommandProperties = {
+  logger: LogHandler
+  cmd: string
+}
+
+const runCommand = ({ cmd, logger }: runCommandProperties) => {
+  return new Promise((resolve, reject) => {
+    logger.debug("going to run command %s", cmd)
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        logger.error("error in running %s", err.message)
+        return reject(err)
+      }
+      if (stderr) {
+        logger.error("unexpected error %s", stderr)
+        return reject(stderr)
+      }
+      resolve(stdout)
+    })
+  })
+}
+
+const runKops = async () => {
+  const logger = getLogger(argv.l)
+  try {
+    let kopsCmd: Array<string | number | undefined> = Array.of("kops ")
+    kopsCmd = [
+      ...kopsCmd,
+      `--cloud=${argv.cp} `,
+      `--name=${argv.cn} `,
+      `--state=${argv.st} `,
+      `--project=${argv.pi} `,
+      `--zones=${argv.z} `,
+      `--ssh-public-key=${argv.sk} `,
+      `--master-size=${argv.ms} `,
+      `--master-count=${argv.mc} `,
+      `--master-volume-size=${argv.mvs} `,
+      `--image=${argv.im} `,
+      `--kubernetes-version=${argv.kv} `,
+      `--node-size=${argv.ns} `,
+      `--node-volume-size=${argv.nvs} `,
+      `--node-count=${argv.nc}`,
+    ]
+    await runCommand({ cmd: kopsCmd.join(""), logger })
+  } catch (error) {
+    throw error
+  }
+}
+
+await runKops()
