8d168e81da3f7d71d12965b9569de835280bc169
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-05-30 18:00:46 -0500

    fix(helm_command_options.ts): change nm option alias to ns to improve semantics
    The nm option alias has been changed to ns to improve semantics and make
    it more intuitive for users.

--- src/deploy/helm_command_options.ts
+++ src/deploy/helm_command_options.ts
@@ -2,7 +2,7 @@ import yargs from "yargs/yargs"
 
 const helmArgv = yargs(process.argv.slice(2))
   .options({
-    nm: {
+    ns: {
       describe: "kubernetes namespace where the chart will be installed",
       type: "string",
       alias: "namespace",
