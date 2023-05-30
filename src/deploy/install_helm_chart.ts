4c51bd11d5e84852e16712d74c962f06abbd3d58
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-05-30 17:17:37 -0500

    refactor(deploy): move yargs command line options to separate file
    The `helm_command_options.ts` file contains the yargs command line
    options for installing a helm chart. The `install_helm_chart.ts` file
    has been updated to import these options from the
    `helm_command_options.ts` file.

--- src/deploy/install_helm_chart.ts
+++ src/deploy/install_helm_chart.ts
@@ -1,9 +1,9 @@
-import yargs from "yargs/yargs"
 import { App, TerraformStack, GcsBackend } from "cdktf"
 import { Construct } from "constructs"
 import { HelmProvider } from "@cdktf/provider-helm/lib/provider"
 import { Release } from "@cdktf/provider-helm/lib/release"
 import { readFileSync } from "fs"
+import { helmArgv as argv } from "./helm_command_options"
 
 type HelmChartProperties = {
   config: string
@@ -53,69 +53,6 @@ class HelmChart extends TerraformStack {
     })
   }
 }
-
-const argv = yargs(process.argv.slice(2))
-  .options({
-    nm: {
-      describe: "kubernetes namespace where the chart will be installed",
-      type: "string",
-      alias: "namespace",
-      demandOption: true,
-    },
-    ch: {
-      describe: "name of the chart",
-      type: "string",
-      alias: "chart",
-      demandOption: true,
-    },
-    repo: {
-      describe: "helm chart reposotiry location",
-      type: "string",
-      alias: "repository",
-      demandOption: true,
-    },
-    kc: {
-      describe: "kubernetes config file",
-      type: "string",
-      demandOption: true,
-      alias: "kubeconfig",
-      default: process.env.KUBECONFIG,
-    },
-    c: {
-      alias: "credentials",
-      description: "service account credentials file for google provider",
-      type: "string",
-      default: "credentials/cloud-manager.json",
-    },
-    bn: {
-      alias: "bucket-name",
-      type: "string",
-      default: "dicty-terraform-state",
-      description: "GCS bucket name where terraform remote state is stored.",
-    },
-    bp: {
-      alias: "bucket-prefix",
-      type: "string",
-      default: "kops-cluster-cdktf",
-      description:
-        "GCS bucket folder prefix where terraform remote state is stored.",
-    },
-    r: {
-      alias: "remote",
-      type: "boolean",
-      default: true,
-      description: "whether the remote gcs backend will be used",
-    },
-    v: {
-      alias: "chart-version",
-      type: "string",
-      describe: "version of the chart to be deployed",
-      demandOption: true,
-    },
-  })
-  .help()
-  .completion()
-  .parseSync()
 
 const app = new App()
 new HelmChart(app, argv.ch, {
