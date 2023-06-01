7f2180b1849c1ffa2d7aa04f442d2e4845470d4d
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-06-01 07:34:48 -0500

    feat(install_pgo.ts): add script to install PostgreSQL Operator using Helm chart
    This commit adds a new file `install_pgo.ts` which contains a script to
    install the PostgreSQL Operator using a Helm chart. The script uses
    `cdktf` to create the necessary infrastructure resources. It takes in
    several command line arguments to configure the installation, such as
    the name of the install, the Kubernetes namespace where the operator
    will be installed, the name of the chart, the repository location of the
    chart, the Kubernetes config file, the service account credentials file
    for Google provider, the GCS bucket name where Terraform remote state is
    stored, the GCS bucket folder prefix where Terraform remote state is
    stored, whether the remote GCS backend will be used, and the version of
    the chart to be deployed.

---
+++ src/deploy/install_pgo.ts
@@ -0,0 +1,87 @@
+import { App } from "cdktf"
+import { HelmChartStack } from "../construct/helm"
+import yargs from "yargs/yargs"
+
+const argv = yargs(process.argv.slice(2))
+  .options({
+    nm: {
+      describe: "name of the pgo install",
+      alias: "name",
+      type: "string",
+      default: "postgresql-operator",
+    },
+    ns: {
+      describe: "kubernetes namespace where the pgo operator will be installed",
+      type: "string",
+      alias: "namespace",
+      default: "operators",
+    },
+    ch: {
+      describe: "name of the chart",
+      type: "string",
+      alias: "chart",
+      default: "pgo",
+    },
+    repo: {
+      describe: "pgo helm chart reposotiry location",
+      type: "string",
+      alias: "repository",
+      default: "oci://registry.developers.crunchydata.com/crunchydata/pgo",
+    },
+    kc: {
+      describe: "kubernetes config file",
+      type: "string",
+      demandOption: true,
+      alias: "kubeconfig",
+      default: process.env.KUBECONFIG,
+    },
+    c: {
+      alias: "credentials",
+      description: "service account credentials file for google provider",
+      type: "string",
+      default: "credentials/cloud-manager.json",
+    },
+    bn: {
+      alias: "bucket-name",
+      type: "string",
+      default: "dicty-terraform-state",
+      description: "GCS bucket name where terraform remote state is stored.",
+    },
+    bp: {
+      alias: "bucket-prefix",
+      type: "string",
+      default: "kops-cluster-cdktf",
+      description:
+        "GCS bucket folder prefix where terraform remote state is stored.",
+    },
+    r: {
+      alias: "remote",
+      type: "boolean",
+      default: true,
+      description: "whether the remote gcs backend will be used",
+    },
+    v: {
+      alias: "chart-version",
+      type: "string",
+      describe: "version of the chart to be deployed",
+      default: "5.3.1",
+    },
+  })
+  .help()
+  .completion()
+  .parseSync()
+
+const app = new App()
+new HelmChartStack(app, argv.nm, {
+  config: argv.kc,
+  version: argv.v,
+  remote: argv.r,
+  credentials: argv.c,
+  bucketName: argv.bn,
+  bucketPrefix: argv.bp,
+  repo: argv.repo,
+  namespace: argv.ns,
+  chart: argv.ch,
+  name: argv.nm,
+})
+app.synth()
