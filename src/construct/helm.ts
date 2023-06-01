378130e3f6333369a680bf25c7a643dab1ac879b
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-06-01 07:16:31 -0500

    refactor(helm.ts): move HelmChartStack class to its own file
    The HelmChartStack class has been moved to its own file, helm.ts, to
    improve code organization and maintainability.

---
+++ src/construct/helm.ts
@@ -0,0 +1,61 @@
+import { TerraformStack, GcsBackend } from "cdktf"
+import { Construct } from "constructs"
+import { HelmProvider } from "@cdktf/provider-helm/lib/provider"
+import { Release, ReleaseSet } from "@cdktf/provider-helm/lib/release"
+import { readFileSync } from "fs"
+
+type HelmChartProperties = {
+  config: string
+  version: string
+  remote: boolean
+  credentials: string
+  bucketName: string
+  bucketPrefix: string
+  repo: string
+  namespace: string
+  chart: string
+  name: string
+  values?: Array<ReleaseSet>
+}
+
+class HelmChartStack extends TerraformStack {
+  constructor(scope: Construct, id: string, options: HelmChartProperties) {
+    const {
+      version,
+      chart,
+      remote,
+      namespace,
+      credentials,
+      bucketName,
+      bucketPrefix,
+      config,
+      repo,
+      name,
+      values,
+    } = options
+    super(scope, id)
+    if (remote) {
+      new GcsBackend(this, {
+        bucket: bucketName,
+        prefix: bucketPrefix,
+        credentials: readFileSync(credentials).toString(),
+      })
+    }
+    new HelmProvider(this, "helm", {
+      kubernetes: {
+        configPath: config,
+      },
+    })
+    new Release(this, id, {
+      name: name,
+      chart: chart,
+      repository: repo,
+      createNamespace: true,
+      namespace,
+      version,
+      set: values,
+    })
+  }
+}
+
+export { HelmChartStack }
--- src/deploy/install_helm_chart.ts
+++ src/deploy/install_helm_chart.ts
@@ -1,63 +1,6 @@
-import { App, TerraformStack, GcsBackend } from "cdktf"
-import { Construct } from "constructs"
-import { HelmProvider } from "@cdktf/provider-helm/lib/provider"
-import { Release, ReleaseSet } from "@cdktf/provider-helm/lib/release"
-import { readFileSync } from "fs"
+import { App } from "cdktf"
 import { helmArgv as argv } from "./helm_command_options"
-
-type HelmChartProperties = {
-  config: string
-  version: string
-  remote: boolean
-  credentials: string
-  bucketName: string
-  bucketPrefix: string
-  repo: string
-  namespace: string
-  chart: string
-  name: string
-  values?: Array<ReleaseSet>
-}
-
-class HelmChartStack extends TerraformStack {
-  constructor(scope: Construct, id: string, options: HelmChartProperties) {
-    const {
-      version,
-      chart,
-      remote,
-      namespace,
-      credentials,
-      bucketName,
-      bucketPrefix,
-      config,
-      repo,
-      name,
-      values,
-    } = options
-    super(scope, id)
-    if (remote) {
-      new GcsBackend(this, {
-        bucket: bucketName,
-        prefix: bucketPrefix,
-        credentials: readFileSync(credentials).toString(),
-      })
-    }
-    new HelmProvider(this, "helm", {
-      kubernetes: {
-        configPath: config,
-      },
-    })
-    new Release(this, id, {
-      name: name,
-      chart: chart,
-      repository: repo,
-      createNamespace: true,
-      namespace,
-      version,
-      set: values,
-    })
-  }
-}
+import { HelmChartStack } from "../construct/helm"
 
 const app = new App()
 new HelmChartStack(app, argv.nm, {
