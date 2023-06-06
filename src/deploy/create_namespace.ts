5c5b00a6b67f031cdbe103d20ee851591381d68e
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-06-06 09:30:55 -0500

    feat(create_namespace.ts): add script to create a Kubernetes namespace using Terraform CDK
    This commit adds a new file `create_namespace.ts` which contains a
    script to create a Kubernetes namespace using Terraform CDK. The script
    uses yargs to parse command line arguments, including the namespace
    name, Kubernetes config file, and options for using a remote GCS backend
    for Terraform state storage. The script creates a new `NamespaceStack`
    construct and passes in the parsed arguments to configure it. Finally,
    the script calls `app.synth()` to generate the Terraform configuration
    files.

---
+++ src/deploy/create_namespace.ts
@@ -0,0 +1,51 @@
+import yargs from "yargs/yargs"
+import { NamespaceStack } from "../construct/namespace"
+import { App } from "cdktf"
+
+const argv = yargs(process.argv.slice(2))
+  .options({
+    ns: {
+      describe: "kubernetes namespace to create",
+      type: "string",
+      alias: "namespace",
+      demandOption: true,
+    },
+    kc: {
+      describe: "kubernetes config file",
+      type: "string",
+      demandOption: true,
+      alias: "kubeconfig",
+    },
+    c: {
+      alias: "credentials",
+      description: "service account credentials file for google provider",
+      type: "string",
+      default: "credentials/cloud-manager.json",
+    },
+    bn: {
+      alias: "bucket-name",
+      type: "string",
+      default: "dicty-terraform-state",
+      description: "GCS bucket name where terraform remote state is stored.",
+    },
+    r: {
+      alias: "remote",
+      type: "boolean",
+      default: true,
+      description: "whether the remote gcs backend will be used",
+    },
+  })
+  .help()
+  .completion()
+  .parseSync()
+
+const app = new App()
+new NamespaceStack(app, argv.ns, {
+  config: argv.kc,
+  remote: argv.r,
+  credentials: argv.c,
+  bucketName: argv.bn,
+  bucketPrefix: argv.ns,
+  namespace: argv.ns,
+})
+app.synth()
