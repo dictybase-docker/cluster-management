6d0841546e1c8020b81cbbb6e0495e28a30160ba
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-06-07 08:17:23 -0500

    feat(install_arangodb_instance.ts): add script to install ArangoDB instance using Terraform CDK
    This commit adds a new script that installs an ArangoDB instance using
    Terraform CDK. The script takes in several command line arguments that
    allow for customization of the installation, such as the Kubernetes
    config file, the name of the installation, the version of ArangoDB to
    install, and the size of the storage in GB. The script uses the
    ArangodbSingle construct to create the ArangoDB instance.

---
+++ src/deploy/install_arangodb_instance.ts
@@ -0,0 +1,86 @@
+import yargs from "yargs/yargs"
+import { ArangodbSingle } from "../construct/arangodb"
+import { App } from "cdktf"
+
+const argv = yargs(process.argv.slice(2))
+  .options({
+    kc: {
+      describe: "kubernetes config file",
+      type: "string",
+      demandOption: true,
+      alias: "kubeconfig",
+    },
+    c: {
+      alias: "credentials",
+      description: "service account credentials file for google provider",
+      type: "string",
+      default: "credentials/cloud-manager.json",
+    },
+    bn: {
+      alias: "bucket-name",
+      type: "string",
+      default: "dicty-terraform-state",
+      description: "GCS bucket name where terraform remote state is stored.",
+    },
+    r: {
+      alias: "remote",
+      type: "boolean",
+      default: true,
+      description: "whether the remote gcs backend will be used",
+    },
+    nm: {
+      describe: "name of the install",
+      alias: "name",
+      type: "string",
+      default: "redis",
+    },
+    ns: {
+      describe: "kubernetes namespace where the chart will be installed",
+      type: "string",
+      alias: "namespace",
+      demandOption: true,
+    },
+    av: {
+      alias: "arangodb-version",
+      type: "string",
+      describe: "redis version to install",
+      default: "3.10.6",
+    },
+    axv: {
+      alias: "arangodb-exporter-version",
+      type: "string",
+      describe: "redis metric exporter version to install",
+      default: "0.1.8",
+    },
+    sc: {
+      alias: "storage-class",
+      type: "string",
+      describe: "name of storage class to use for persistence",
+      default: "dictycr-balanced",
+    },
+    ss: {
+      alias: "storage-size",
+      type: "number",
+      describle: "size of the storage in GB",
+      default: 30,
+    },
+  })
+  .help()
+  .completion()
+  .parseSync()
+
+const app = new App()
+new ArangodbSingle(app, argv.nm, {
+  config: argv.kc,
+  redisVersion: argv.rv,
+  redisExporterVersion: argv.rxv,
+  remote: argv.r,
+  credentials: argv.c,
+  bucketName: argv.bn,
+  bucketPrefix: argv.bp,
+  namespace: argv.ns,
+  name: argv.nm,
+  storageClass: argv.sc,
+  storageSize: argv.ss,
+})
+app.synth()
