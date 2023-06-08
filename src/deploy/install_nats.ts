0131c0b0d2707529bb2811576e55ef96c67e5c6a
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-06-08 14:53:52 -0500

    feat(install_nats.ts): add script to install nats using Helm chart
    This commit adds a new script to install nats using a Helm chart. The
    script takes in several command line arguments to configure the
    installation, including the name of the nats install, the kubernetes
    namespace where the nats operator will be installed, the name of the
    chart, the repository location of the nats helm chart, the kubernetes
    config file, the service account credentials file for the google
    provider, the GCS bucket name where terraform remote state is stored,
    whether the remote GCS backend will be used, the version of the chart to
    be deployed, and the nats version. The script uses the cdktf library to
    create a new HelmChartStack and deploys the nats chart with the
    specified configuration.

---
+++ src/deploy/install_nats.ts
@@ -0,0 +1,87 @@
+import { App } from "cdktf"
+import { HelmChartStack } from "../construct/helm"
+import yargs from "yargs/yargs"
+
+const argv = yargs(process.argv.slice(2))
+  .options({
+    nm: {
+      describe: "name of the nats install",
+      alias: "name",
+      type: "string",
+      default: "nats",
+    },
+    ns: {
+      describe: "kubernetes namespace where the pgo operator will be installed",
+      type: "string",
+      alias: "namespace",
+      demandOption: true,
+    },
+    ch: {
+      describe: "name of the chart",
+      type: "string",
+      alias: "chart",
+      default: "nats",
+    },
+    repo: {
+      describe: "nats helm chart reposotiry location",
+      type: "string",
+      alias: "repository",
+      default: "https://nats-io.github.io/k8s/helm/charts",
+    },
+    kc: {
+      describe: "kubernetes config file",
+      type: "string",
+      demandOption: true,
+      alias: "kubeconfig",
+    },
+    c: {
+      alias: "credentials",
+      description: "service account credentials file for google provider",
+      type: "string",
+      default: "credentials/cloud-manager.json",
+    },
+    bn: {
+      alias: "bucket-name",
+      type: "string",
+      default: "dicty-terraform-state",
+      description: "GCS bucket name where terraform remote state is stored.",
+    },
+    r: {
+      alias: "remote",
+      type: "boolean",
+      default: true,
+      description: "whether the remote gcs backend will be used",
+    },
+    v: {
+      alias: "chart-version",
+      type: "string",
+      describe: "version of the chart to be deployed",
+      default: "0.19.14",
+    },
+    nv: {
+      alias: "nats-version",
+      type: "string",
+      describe: "nats version",
+      default: "2.9.17-alpine",
+    },
+  })
+  .help()
+  .completion()
+  .parseSync()
+
+const app = new App()
+const deployName = argv.nm.concat("-").concat(argv.ns)
+new HelmChartStack(app, deployName, {
+  config: argv.kc,
+  version: argv.v,
+  remote: argv.r,
+  credentials: argv.c,
+  bucketName: argv.bn,
+  bucketPrefix: deployName,
+  repo: argv.repo,
+  namespace: argv.ns,
+  chart: argv.ch,
+  name: argv.nm,
+  values: [{ name: "nats.image.tag", value: argv.nv }],
+})
+app.synth()
