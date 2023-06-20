a114e45497c24d1f79bde7992724529372ffa93a
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-06-20 12:27:00 -0500

    feat(bucket.ts): add BucketStack class to create a Google Cloud Storage bucket
    The BucketStack class creates a Google Cloud Storage bucket using the
    @cdktf/provider-google library. The class takes in a Provider object
    containing the credentials, project ID, region, and zone, a Resource
    object containing the bucket name, and a Backend object containing the
    configuration for the backend. If the backend is remote, it creates a
    GcsBackend. The StorageBucket resource is created with the specified
    bucket name, location, and versioning enabled.

---
+++ src/construct/bucket.ts
@@ -0,0 +1,60 @@
+import { Construct } from "constructs"
+import { TerraformStack, GcsBackend } from "cdktf"
+import { readFileSync } from "fs"
+import { StorageBucket } from "@cdktf/provider-google/lib/storage-bucket"
+import { GoogleProvider } from "@cdktf/provider-google/lib/provider"
+
+type Backend = {
+  config: string
+  remote: boolean
+  credentials: string
+  bucketName: string
+  bucketPrefix: string
+}
+
+type Provider = {
+  credentials: string
+  projectId: string
+  region: string
+  zone: string
+}
+
+type Resource = {
+  bucketName: string
+}
+
+type BucketStackProperties = {
+  provider: Provider
+  resource: Resource
+  backend: Backend
+}
+
+class BucketStack extends TerraformStack {
+  constructor(scope: Construct, id: string, options: BucketStackProperties) {
+    super(scope, id)
+    const { provider, resource, backend } = options
+    if (backend.remote) {
+      new GcsBackend(this, {
+        bucket: backend.bucketName,
+        prefix: backend.bucketPrefix,
+        credentials: readFileSync(backend.credentials).toString(),
+      })
+    }
+    new GoogleProvider(this, `${id}-google-provider`, {
+      credentials: readFileSync(provider.credentials).toString(),
+      project: provider.projectId,
+      region: provider.region,
+      zone: provider.zone,
+    })
+    new StorageBucket(this, `${id}-bucket`, {
+      forceDestroy: true,
+      name: resource.bucketName,
+      location: "US",
+      versioning: {
+        enabled: true,
+      },
+    })
+  }
+}
+
+export { BucketStack }
