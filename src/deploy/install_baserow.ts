06930dc713af08d8e12163eb7218e25088a272ad
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-06-22 18:27:10 -0500

    feat(install_baserow.ts): add script to install Baserow on Kubernetes cluster
    This script installs Baserow on a Kubernetes cluster using Helm charts.
    It retrieves the necessary credentials from a Kubernetes secret and sets
    the appropriate values in the Helm chart. The script also allows for
    customization of the installation by specifying various options such as
    the name of the installation, the namespace, the chart version, the
    repository location, and the storage class name.

---
+++ src/deploy/install_baserow.ts
@@ -0,0 +1,141 @@
+import yargs from "yargs/yargs"
+import { KubeConfig, CoreV1Api } from "@kubernetes/client-node"
+import { Buffer } from "buffer"
+import { HelmChartStack } from "../construct/helm"
+import { App } from "cdktf"
+
+const decodeSecretData = (value: string) =>
+  Buffer.from(value, "base64").toString("utf8")
+
+const getSecret = async (config: string, secret: string, namespace: string) => {
+  const kubeconfig = new KubeConfig()
+  kubeconfig.loadFromFile(config)
+  const k8sApi = kubeconfig.makeApiClient(CoreV1Api)
+  const res = await k8sApi.listNamespacedSecret(namespace)
+  return res.body.items.find((sec) => sec.metadata?.name === secret)
+}
+
+const argv = yargs(process.argv.slice(2))
+  .options({
+    nm: {
+      describe: "name of the baserow install",
+      alias: "name",
+      type: "string",
+      default: "baserow",
+    },
+    ns: {
+      describe: "kubernetes namespace where the baserow will be installed",
+      type: "string",
+      alias: "namespace",
+      demandOption: true,
+    },
+    ch: {
+      describe: "name of the chart",
+      type: "string",
+      alias: "chart",
+      default: "baserow",
+    },
+    repo: {
+      describe: "baserow helm chart reposotiry location",
+      type: "string",
+      alias: "repository",
+      default: "https://christianknell.github.io/helm-charts",
+    },
+    c: {
+      alias: "credentials",
+      description: "service account credentials file for google provider",
+      type: "string",
+      default: "credentials/cloud-manager.json",
+    },
+    bn: {
+      alias: "bucket-name",
+      type: "string",
+      default: "dicty-terraform-state",
+      description: "GCS bucket name where terraform remote state is stored.",
+    },
+    r: {
+      alias: "remote",
+      type: "boolean",
+      default: true,
+      description: "whether the remote gcs backend will be used",
+    },
+    v: {
+      alias: "chart-version",
+      type: "string",
+      describe: "version of the chart to be deployed",
+      default: "1.18.5",
+    },
+    kc: {
+      describe: "kubernetes config file",
+      type: "string",
+      demandOption: true,
+      alias: "kubeconfig",
+    },
+    s: {
+      alias: "name",
+      type: "string",
+      demandOption: true,
+      describe:
+        "name of the secret from where postgres credentials will be extracted",
+    },
+    sc: {
+      alias: "storage-class",
+      type: "string",
+      default: "dictycr-balanced",
+      describle: "storage class name to use with baserow",
+    },
+  })
+  .help()
+  .completion()
+  .parseSync()
+
+const secret = await getSecret(argv.kc, argv.s, argv.ns)
+const app = new App()
+const deployName = argv.nm.concat("-").concat(argv.ns)
+new HelmChartStack(app, deployName, {
+  config: argv.kc,
+  version: argv.v,
+  remote: argv.r,
+  credentials: argv.c,
+  bucketName: argv.bn,
+  bucketPrefix: deployName,
+  repo: argv.repo,
+  namespace: argv.ns,
+  chart: argv.ch,
+  name: argv.nm,
+  values: [
+    { name: "persistence.enabled", value: "true" },
+    { name: "persistence.storageClassName", value: argv.sc },
+    { name: "postgresql.enabled", value: "false" },
+    {
+      name: "externalPostgresql.auth.database",
+      value: decodeSecretData(secret?.data?.dbname as string),
+    },
+    {
+      name: "externalPostgresql.auth.password",
+      value: decodeSecretData(secret?.data?.password as string),
+    },
+    {
+      name: "externalPostgresql.auth.username",
+      value: decodeSecretData(secret?.data?.user as string),
+    },
+    {
+      name: "externalPostgresql.port",
+      value: decodeSecretData(secret?.data?.port as string),
+    },
+    {
+      name: "externalPostgresql.hostname",
+      value: decodeSecretData(secret?.data?.host as string),
+    },
+    { name: "redis.enabled", value: "false" },
+    {
+      name: "externalRedis.hostname",
+      value: "redis",
+    },
+    {
+      name: "externalRedis.auth.enabled",
+      value: "false",
+    },
+  ],
+})
+app.synth()
