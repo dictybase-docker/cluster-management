dc2e3d1fd7a7424e21ff9073165bf09d89f3dab2
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-07-09 14:59:33 -0500

    feat(restic.ts): refactor ResticSecretStack to RepositoryStack and update constructor and methods to reflect changes in resource properties and container configuration
    The import statement for Secret has been changed to Job to match the
    correct module. The ResticSecretStack class has been refactored to
    RepositoryStack to better reflect its purpose.

--- src/construct/restic.ts
+++ src/construct/restic.ts
@@ -1,7 +1,7 @@
 import { TerraformStack, GcsBackend } from "cdktf"
 import { Construct } from "constructs"
 import { KubernetesProvider } from "@cdktf/provider-kubernetes/lib/provider"
-import { Secret } from "@cdktf/provider-kubernetes/lib/secret"
+import { Job } from "@cdktf/provider-kubernetes/lib/job"
 import { readFileSync } from "fs"
 
 type Provider = {
@@ -11,28 +11,34 @@ type Provider = {
   bucketName: string
   bucketPrefix: string
 }
+
 type Resource = {
+  secretName: string
   namespace: string
-  gcsKey: string
-  project: string
-  resticName: string
-  resticPassword: string
+  image: string
+  backupBucketName: string
 }
-type ResticSecretStackProperties = {
+
+type RepositoryStackProperties = {
   provider: Provider
   resource: Resource
 }
+type containersProperties = {
+  name: string
+  secretName: string
+  image: string
+  bucketName: string
+}
 
-class ResticSecretStack extends TerraformStack {
-  public readonly secret: Secret
+class RepositoryStack extends TerraformStack {
   constructor(
     scope: Construct,
     id: string,
-    options: ResticSecretStackProperties,
+    options: RepositoryStackProperties,
   ) {
     const {
       provider: { remote, credentials, bucketName, bucketPrefix, config },
-      resource: { gcsKey, project, resticName, resticPassword, namespace },
+      resource: { image, namespace, secretName, backupBucketName },
     } = options
     super(scope, id)
     if (remote) {
@@ -47,16 +53,142 @@ class ResticSecretStack extends TerraformStack {
       name: id,
       namespace: namespace,
     }
-    this.secret = new Secret(this, id, {
+    new Job(this, id, {
       metadata,
-      data: {
-        "gcsbucket.credentials": readFileSync(gcsKey).toString(),
-        "gcs.project": project,
-        "restic.name": resticName,
-        "restic.password": resticPassword,
+      spec: {
+        backoffLimit: 0,
+        template: {
+          metadata: this.#template_metadata(`${id}-template`),
+          spec: {
+            volume: this.#volumes(`${id}-volumes`, secretName),
+            restartPolicy: "Never",
+            container: this.#containers({
+              name: `${id}-container`,
+              bucketName: backupBucketName,
+              secretName,
+              image,
+            }),
+          },
+        },
       },
     })
   }
+  #containers({ name, secretName, image, bucketName }: containersProperties) {
+    return [
+      {
+        name,
+        image,
+        command: ["restic", "-r", `gs:${bucketName}/`, "init"],
+        volumeMounts: this.#volumeMounts(`${name}-secret-volume`),
+        env: [
+          ...this.#env(secretName),
+          {
+            name: "GOOGLE_APPLICATION_CREDENTIALS",
+            value: "/var/secret/credentials.json",
+          },
+        ],
+      },
+    ]
+  }
+  #env(secretName: string) {
+    return [
+      { name: "RESTIC_REPOSITORY", key: "restic.name" },
+      { name: "RESTIC_PASSWORD", key: "restic.password" },
+      { name: "GOOGLE_PROJECT_ID", key: "gcs.project" },
+    ].map(({ name, key }) => {
+      return {
+        name,
+        valueFrom: {
+          secretKeyRef: {
+            key,
+            name: secretName,
+          },
+        },
+      }
+    })
+  }
+  #volumeMounts(name: string) {
+    return [{ name, mountPath: "/var/secret" }]
+  }
+  #template_metadata(name: string) {
+    return { name }
+  }
+  #volumes(name: string, secretName: string) {
+    return [
+      {
+        name: name,
+        secret: {
+          secretName,
+          items: [
+            {
+              key: "gcsbucket.credentials",
+              path: "credentials.json",
+            },
+          ],
+        },
+      },
+    ]
+  }
 }
 
-export { ResticSecretStack }
+/* apiVersion: batch/v1
+kind: Job
+metadata:
+  name: my-job
+spec:
+  template:
+    spec:
+      containers:
+        - name: my-container
+          image: my-image
+          env:
+            - name: GOOGLE_APPLICATION_CREDENTIALS
+              valueFrom:
+                secretKeyRef:
+                  name: my-secrets
+                  key: gcsbucket.credentials
+            - name: GOOGLE_PROJECT_ID
+              valueFrom:
+                secretKeyRef:
+                  name: my-secrets
+                  key: gcs.project
+            - name: RESTIC_REPOSITORY
+              valueFrom:
+                secretKeyRef:
+                  name: my-secrets
+                  key: restic.name
+            - name: RESTIC_PASSWORD
+              valueFrom:
+                secretKeyRef:
+                  name: my-secrets
+                  key: restic.password
+      restartPolicy: Never
+  backoffLimit: 0 */
+
+/* apiVersion: batch/v1
+kind: Job
+metadata:
+  name: my-job
+spec:
+  template:
+    spec:
+      containers:
+        - name: my-container
+          image: my-image
+          env:
+            - name: GOOGLE_APPLICATION_CREDENTIALS
+              value: /var/secret/credentials.json
+          volumeMounts:
+            - name: secret-volume
+              mountPath: /var/secret
+      restartPolicy: Never
+  backoffLimit: 0
+  volumes:
+    - name: secret-volume
+      secret:
+        secretName: dictybase.staging
+        items:
+          - key: gcsbucket.credentials
+            path: credentials.json */
+
+export { RepositoryStack }
