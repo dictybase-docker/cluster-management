e2385aefe6549de61321109b50c7d147afce2f42
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-07-10 16:04:55 -0500

    feat(create_restic_repo.ts): add script to create a Restic repository stack
    The create_restic_repo.ts script is added to the project. This script
    uses yargs to parse command line arguments and creates a Restic
    repository stack using the RepositoryStack construct from the
    ../construct/restic module. The script takes various options such as the
    name of the installation, the Kubernetes namespace, the Kubernetes
    config file, the GCS bucket name for Terraform remote state, whether to
    use a remote GCS backend, the GCS bucket for backup, the dictycr secret
    containing the Restic credentials, and the image to use for the backup
    job. The script creates an instance of the App class, constructs the
    deployName using the provided options, and creates a new instance of the
    RepositoryStack with the necessary configuration options. Finally, the
    app.synth() method is called to synthesize the stack. This script is
    added to enable the creation of a Restic repository stack for backup
    purposes.

---
+++ src/deploy/create_restic_repo.ts
@@ -0,0 +1,83 @@
+import yargs from "yargs/yargs"
+import { App } from "cdktf"
+import { RepositoryStack } from "../construct/restic"
+
+const argv = yargs(process.argv.slice(2))
+  .options({
+    nm: {
+      type: "string",
+      alias: "name",
+      describe: "name of the this install",
+      demandOption: true,
+    },
+    ns: {
+      describe: "kubernetes namespace to install",
+      type: "string",
+      alias: "namespace",
+      demandOption: true,
+    },
+    kc: {
+      describe: "kubernetes config file",
+      type: "string",
+      demandOption: true,
+      alias: "kubeconfig",
+    },
+    c: {
+      alias: "credentials",
+      description: "service account credentials file for gcs backend",
+      type: "string",
+      default: "credentials/cloud-manager.json",
+    },
+    bn: {
+      alias: "bucket-name",
+      type: "string",
+      default: "dicty-terraform-state",
+      description: "GCS bucket name where terraform remote state is stored.",
+    },
+    r: {
+      alias: "remote",
+      type: "boolean",
+      default: true,
+      description: "whether the remote gcs backend will be used",
+    },
+    bb: {
+      alias: "backup-bucket",
+      type: "string",
+      demandOption: true,
+      describe: "gcs bucket for backup",
+    },
+    ds: {
+      alias: "dictyr-secret",
+      describe: "dictycr secret which contains the restic credentials",
+      type: "string",
+      demandOption: true,
+    },
+    im: {
+      alias: "backup-image",
+      describe: "image to use for backup job",
+      type: "string",
+      default: "dictybase/resticpg:e9b04a2d3cc4",
+    },
+  })
+  .help()
+  .completion()
+  .parseSync()
+
+const app = new App()
+const deployName = argv.nm.concat("-").concat(argv.ns)
+new RepositoryStack(app, deployName, {
+  provider: {
+    config: argv.kc,
+    remote: argv.r,
+    credentials: argv.c,
+    bucketName: argv.bn,
+    bucketPrefix: deployName,
+  },
+  resource: {
+    namespace: argv.ns,
+    secretName: argv.ds,
+    image: argv.im,
+    backupBucketName: argv.bb,
+  },
+})
+app.synth()
