e622499f65005c2623467706ba8a355b46de02c4
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-07-18 17:01:01 -0500

    feat(deploy): add script to install ArangoDB database
    This commit adds a new file `install_arangodb_database.ts` which contains a script to install an ArangoDB database. The script uses yargs to parse command line arguments and accepts various options such as the name of the installation, the Kubernetes namespace to install, the Kubernetes config file, service account credentials file for GCS backend, GCS bucket name for storing Terraform remote state, whether to use a remote GCS backend, ArangoDB root user, ArangoDB admin user password, ArangoDB user to be created, default database permission for the newly created user, image to use for database and user creations, and the ArangoDB databases to create.
    
    The script creates a new instance of the `App` class from the `cdktf` library and initializes a new `DatabaseStack` with the provided options. The `DatabaseStack` is responsible for creating the necessary resources for the ArangoDB database installation. Finally, the `app.synth()` method is called to synthesize the Terraform code.
    
    The purpose of this change is to automate the installation of an ArangoDB database by providing a script that can be executed with the necessary options.

---
+++ src/deploy/install_arangodb_database.ts
@@ -0,0 +1,104 @@
+import yargs from "yargs/yargs"
+import { App } from "cdktf"
+import { DatabaseStack } from "../construct/arangodb"
+
+const argv = yargs(process.argv.slice(2))
+  .options({
+    nm: {
+      type: "string",
+      alias: "name",
+      describe: "name of the this install",
+      demandOption: true,
+    },
+    ns: {
+      describe: "kubernetes namespace to install",
+      type: "string",
+      alias: "namespace",
+      demandOption: true,
+    },
+    kc: {
+      describe: "kubernetes config file",
+      type: "string",
+      demandOption: true,
+      alias: "kubeconfig",
+    },
+    c: {
+      alias: "credentials",
+      description: "service account credentials file for gcs backend",
+      type: "string",
+      default: "credentials/cloud-manager.json",
+    },
+    bn: {
+      alias: "bucket-name",
+      type: "string",
+      default: "dicty-terraform-state",
+      description: "GCS bucket name where terraform remote state is stored.",
+    },
+    r: {
+      alias: "remote",
+      type: "boolean",
+      default: true,
+      description: "whether the remote gcs backend will be used",
+    },
+    au: {
+      alias: "admin-user",
+      type: "string",
+      describe: "arangodb root user",
+      default: "root",
+    },
+    ap: {
+      alias: "admin-pass",
+      type: "string",
+      describe: "arangodb admin user password",
+      demandOption: true,
+    },
+    us: {
+      alias: "user",
+      type: "string",
+      describe: "arangodb user to be created",
+      demandOption: true,
+    },
+    gr: {
+      alias: "grant",
+      type: "string",
+      default: "rw",
+      describe: "default database permission for the newly created user",
+    },
+    im: {
+      alias: "image",
+      describe: "image to use for database and user creations",
+      type: "string",
+      default: "dictybase/arangoadmin:develop-cdf7c20",
+    },
+    dbs: {
+      alias: "databases",
+      describe: "arangodb databases to create",
+      type: "array",
+      demandOption: true,
+    },
+  })
+  .help()
+  .completion()
+  .parseSync()
+
+const app = new App()
+const deployName = argv.ns.concat("-").concat(argv.nm)
+new DatabaseStack(app, deployName, {
+  provider: {
+    config: argv.kc,
+    remote: argv.r,
+    credentials: argv.c,
+    bucketName: argv.bn,
+    bucketPrefix: deployName,
+  },
+  resource: {
+    namespace: argv.ns,
+    image: argv.im,
+    adminUser: argv.au,
+    adminPassword: argv.ap,
+    user: argv.us,
+    grant: argv.gr,
+    databases: argv.dbs as Array<string>,
+  },
+})
+app.synth()
