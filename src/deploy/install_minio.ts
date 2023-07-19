c25a343d1ef5d7ddf7a407ffc7941fd3558a6040
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-07-19 18:13:31 -0500

    feat(install_minio.ts): add script to install Minio using Helm chart
    The `install_minio.ts` script is added to the project. This script
    allows for the installation of Minio using a Helm chart. It provides
    various options such as specifying the name of the Minio install, the
    Kubernetes namespace, the chart name, the repository location, the
    Kubernetes config file, the service account credentials file, the GCS
    bucket name for storing Terraform remote state, whether to use the
    remote GCS backend, and the version of the chart to be deployed. The
    script creates a new instance of the `HelmChartStack` construct and sets
    the necessary configuration options. Finally, the script synthesizes the
    CDK app.

---
+++ src/deploy/install_minio.ts
@@ -0,0 +1,85 @@
+import { App } from "cdktf"
+import { HelmChartStack } from "../construct/helm"
+import yargs from "yargs/yargs"
+
+const argv = yargs(process.argv.slice(2))
+  .options({
+    nm: {
+      describe: "name of the minio install",
+      alias: "name",
+      type: "string",
+      default: "minio",
+    },
+    ns: {
+      describe: "kubernetes namespace where the pgo operator will be installed",
+      type: "string",
+      alias: "namespace",
+      demandOption: true,
+    },
+    ch: {
+      describe: "name of the chart",
+      type: "string",
+      alias: "chart",
+      default: "minio",
+    },
+    repo: {
+      describe: "nats helm chart reposotiry location",
+      type: "string",
+      alias: "repository",
+      default: "https://charts.min.io",
+    },
+    kc: {
+      describe: "kubernetes config file",
+      type: "string",
+      demandOption: true,
+      alias: "kubeconfig",
+    },
+    c: {
+      alias: "credentials",
+      description: "service account credentials file for google provider",
+      type: "string",
+      default: "credentials/cloud-manager.json",
+    },
+    bn: {
+      alias: "bucket-name",
+      type: "string",
+      default: "dicty-terraform-state",
+      description: "GCS bucket name where terraform remote state is stored.",
+    },
+    r: {
+      alias: "remote",
+      type: "boolean",
+      default: true,
+      description: "whether the remote gcs backend will be used",
+    },
+    ver: {
+      alias: "chart-version",
+      type: "string",
+      describe: "version of the chart to be deployed",
+      default: "5.0.13",
+    },
+  })
+  .help()
+  .completion()
+  .parseSync()
+
+const app = new App()
+const deployName = argv.nm.concat("-").concat(argv.ns)
+new HelmChartStack(app, deployName, {
+  config: argv.kc,
+  version: argv.ver,
+  remote: argv.r,
+  credentials: argv.c,
+  bucketName: argv.bn,
+  bucketPrefix: deployName,
+  repo: argv.repo,
+  namespace: argv.ns,
+  chart: argv.ch,
+  name: argv.nm,
+  values: [
+    { name: "mode", value: "standalone" },
+    { name: "persistence.size", value: "50Gi" },
+    { name: "persistence.storageClass", value: "dictycr-balanced" },
+  ],
+})
+app.synth()
