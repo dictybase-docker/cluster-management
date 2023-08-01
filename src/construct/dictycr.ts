cd5e2da10d93f14062b9a9d9ab0a9c3fc99ab30b
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-08-01 15:56:46 -0500

    feat(dictycr.ts): add support for configuring container properties in BackendDeployment class
    The BackendDeployment class in dictycr.ts now supports configuring
    container properties such as name, image, database, secretName, service,
    and port.

--- src/construct/dictycr.ts
+++ src/construct/dictycr.ts
@@ -30,10 +30,23 @@ type BackendDeploymentResource = {
   namespace: string
   image: string
   tag: string
+  database: string
+  secretName: string
+  service: string
+  port: number
 }
 type BackendDeploymentProperties = {
   provider: Provider
   resource: BackendDeploymentResource
+}
+
+type containerProperties = {
+  name: string
+  imageWithTag: string
+  database: string
+  secretName: string
+  service: string
+  port: number
 }
 
 class SecretStack extends TerraformStack {
@@ -87,7 +100,7 @@ class BackendDeployment extends TerraformStack {
   ) {
     const {
       provider: { remote, credentials, bucketName, bucketPrefix, config },
-      resource: { namespace },
+      resource: { namespace, image, tag, database, secretName, service, port },
     } = options
     super(scope, id)
     if (remote) {
@@ -98,18 +111,86 @@ class BackendDeployment extends TerraformStack {
       })
     }
     new KubernetesProvider(this, `${id}-provider`, { configPath: config })
+    new Deployment(this, id, {
+      metadata: this.#metadata(id, namespace),
+      spec: {
+        selector: {
+          matchLabels: {
+            app: id,
+          },
+        },
+        template: {
+          metadata: { labels: { app: `${id}-template` } },
+          spec: {
+            container: this.#containers({
+              name: `${id}-container`,
+              imageWithTag: `${image}:${tag}`,
+              database,
+              secretName,
+              service,
+              port,
+            }),
+          },
+        },
+      },
+    })
   }
   #metadata(name: string, namespace: string) {
     return { name, namespace }
   }
-  #containers() {
-    return []
+  #containers({
+    name,
+    imageWithTag,
+    database,
+    secretName,
+    service,
+    port,
+  }: containerProperties) {
+    return [
+      {
+        name,
+        image: imageWithTag,
+        args: this.#commandArgs(database),
+        env: this.#env(secretName),
+        ports: this.#ports(service, port),
+      },
+    ]
   }
-  #commandArgs() {
-    return []
+  #commandArgs(database: string) {
+    return [
+      "start-server",
+      "--user",
+      "$(ARANGODB_USER)",
+      "--pass",
+      "$(ARANGODB_PASSWORD)",
+      "--db",
+      database,
+    ]
   }
-  #env() {
-    return []
+  #env(secretName: string) {
+    return [
+      {
+        name: "ARANGODB_PASSWORD",
+        valueFrom: {
+          secretKeyRef: {
+            name: secretName,
+            key: "arangodb.password",
+          },
+        },
+      },
+      {
+        name: "ARANGODB_USER",
+        valueFrom: {
+          secretKeyRef: {
+            name: secretName,
+            key: "arangodb.user",
+          },
+        },
+      },
+    ]
+  }
+  #ports(service: string, port: number) {
+    return [{ name: service, containerPort: port, protocol: "TCP" }]
   }
 }
 
