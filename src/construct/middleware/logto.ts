bdd8b86ff6ce28ab6a5f273ed7c3c87b2188798e
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-10-11 18:32:03 -0500

    feat(logto.ts): add LogtoBackendDeploymentStack class to create a deployment for the Logto backend
    The LogtoBackendDeploymentStack class is added to create a deployment
    for the Logto backend. This class takes in various properties such as
    the provider configuration, resource details, and port properties to
    configure the deployment. It uses the TerraformStack, GcsBackend,
    KubernetesProvider, and Deployment constructs from the cdktf library to
    create the deployment. The deployment includes containers with specific
    configurations, environment variables sourced from a config map, and
    exposed ports. This class provides a convenient way to create and manage
    the deployment for the Logto backend.

---
+++ src/construct/middleware/logto.ts
@@ -0,0 +1,151 @@
+import { TerraformStack, GcsBackend } from "cdktf"
+import { Construct } from "constructs"
+import { KubernetesProvider } from "@cdktf/provider-kubernetes/lib/provider"
+import { Deployment } from "@cdktf/provider-kubernetes/lib/deployment"
+import { readFileSync } from "fs"
+
+type Provider = {
+  config: string
+  remote: boolean
+  credentials: string
+  bucketName: string
+  bucketPrefix: string
+}
+
+type LogtoBackendDeploymentResource = {
+  namespace: string
+  image: string
+  tag: string
+  configMapname: string
+  service: string
+  adminPort: number
+  apiPort: number
+  origins: Array<string>
+}
+
+type LogtoBackendDeploymentProperties = {
+  provider: Provider
+  resource: LogtoBackendDeploymentResource
+}
+
+type portPropterties = Pick<
+  LogtoBackendDeploymentResource,
+  "service" | "apiPort" | "adminPort"
+>
+
+type containerProperties = portPropterties & {
+  name: string
+  configMapname: string
+  image: string
+  tag: string
+}
+
+class LogtoBackendDeploymentStack extends TerraformStack {
+  constructor(
+    scope: Construct,
+    id: string,
+    options: LogtoBackendDeploymentProperties,
+  ) {
+    const {
+      provider: { remote, credentials, bucketName, bucketPrefix, config },
+      resource: {
+        namespace,
+        image,
+        tag,
+        configMapname,
+        service,
+        adminPort,
+        apiPort,
+      },
+    } = options
+    super(scope, id)
+    if (remote) {
+      new GcsBackend(this, {
+        bucket: bucketName,
+        prefix: bucketPrefix,
+        credentials: readFileSync(credentials).toString(),
+      })
+    }
+    new KubernetesProvider(this, `${id}-provider`, { configPath: config })
+    new Deployment(this, id, {
+      metadata: this.#metadata(id, namespace),
+      spec: {
+        selector: {
+          matchLabels: {
+            app: id,
+          },
+        },
+        template: {
+          metadata: { labels: { app: id } },
+          spec: {
+            container: this.#containers({
+              name: `${id}-container`,
+              image,
+              tag,
+              configMapname,
+              service,
+              adminPort,
+              apiPort,
+            }),
+          },
+        },
+      },
+    })
+  }
+  #metadata(name: string, namespace: string) {
+    return { name, namespace }
+  }
+  #containers({
+    name,
+    image,
+    configMapname,
+    service,
+    adminPort,
+    apiPort,
+    tag,
+  }: containerProperties) {
+    return [
+      {
+        name,
+        image: `${image}:${tag}`,
+        command: ["/bin/sh"],
+        args: [
+          "-c",
+          `npm run alteration deploy ${tag} && npm run cli db seed -- --swe && npm start`,
+        ],
+        env: this.#env(configMapname),
+        port: this.#ports({ service, adminPort, apiPort }),
+      },
+    ]
+  }
+  #env(configMapname: string) {
+    return [
+      {
+        name: "PUBLICATION_API_ENDPOINT",
+        valueFrom: {
+          configMapKeyRef: {
+            name: configMapname,
+            key: "endpoint.publication",
+          },
+        },
+      },
+      {
+        name: "ORGANISM_API_ENDPOINT",
+        valueFrom: {
+          configMapKeyRef: {
+            name: configMapname,
+            key: "endpoint.organism",
+          },
+        },
+      },
+    ]
+  }
+  #ports({ service, adminPort, apiPort }: portPropterties) {
+    return [
+      { name: `${service}-admin`, containerPort: adminPort, protocol: "TCP" },
+      { name: `${service}-api`, containerPort: apiPort, protocol: "TCP" },
+    ]
+  }
+}
+
+export { LogtoBackendDeploymentStack }
