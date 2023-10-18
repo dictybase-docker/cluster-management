09655989b721a0878d32e15ef11678303313e29f
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-10-18 14:31:39 -0500

    refactor(install_logto.ts): improve code readability and maintainability by removing unused imports and refactoring command line parsing logic
    The changes in this commit remove the unused import of `yargs` and
    refactor the command line parsing logic in the `install_logto.ts` file.
    This improves the code readability and maintainability by removing
    unnecessary code and making the command line parsing logic more concise
    and modular.

--- src/deploy/middleware/install_logto.ts
+++ src/deploy/middleware/install_logto.ts
@@ -1,4 +1,3 @@
-import yargs from "yargs/yargs"
 import { V1Secret } from "@kubernetes/client-node"
 import { App } from "cdktf"
 import {
@@ -7,172 +6,33 @@ import {
 } from "../../construct/middleware/logto"
 import { getSecret } from "../../k8s"
 import { BackendService } from "../../construct/dictycr"
+import {
+  process_logto_cmdline,
+  pvc,
+  apiService,
+  adminService,
+  logtoPvcStackOptions,
+  logtoBackendDeploymentOptions,
+  logtoServiceOptions,
+} from "./logto_cli"
 
-const argv = yargs(process.argv.slice(2))
-  .options({
-    kc: {
-      describe: "kubernetes config file",
-      type: "string",
-      demandOption: true,
-      alias: "kubeconfig",
-    },
-    c: {
-      alias: "credentials",
-      description: "service account credentials file for gcs backend",
-      type: "string",
-      default: "credentials/cloud-manager.json",
-    },
-    bn: {
-      alias: "bucket-name",
-      type: "string",
-      default: "dicty-terraform-state",
-      description: "GCS bucket name where terraform remote state is stored.",
-    },
-    r: {
-      alias: "remote",
-      type: "boolean",
-      default: true,
-      description: "whether the remote gcs backend will be used",
-    },
-    nm: {
-      type: "string",
-      alias: "name",
-      default: "logto",
-      describe: "name of the this install",
-    },
-    ns: {
-      describe: "kubernetes namespace to install",
-      type: "string",
-      alias: "namespace",
-      demandOption: true,
-    },
-    im: {
-      alias: "image",
-      describe: "image to use for logto",
-      type: "string",
-      default: "svhd/logto",
-    },
-    tg: {
-      alias: "tag",
-      describe: "tag of the image",
-      type: "string",
-      demandOption: true,
-    },
-    sc: {
-      alias: "storage-class",
-      type: "string",
-      describe: "name of storage class to use for persistence",
-      default: "dictycr-balanced",
-    },
-    ss: {
-      alias: "storage-size",
-      type: "number",
-      describle: "size of the storage in GB",
-      default: 30,
-    },
-    sr: {
-      alias: "postgres-secret",
-      type: "string",
-      demandOption: true,
-      describe:
-        "name of the secret from where postgres credentials will be extracted",
-    },
-    adp: {
-      alias: "admin-port",
-      type: "number",
-      default: 3002,
-      description: "port where admin console will be listening",
-    },
-    ap: {
-      alias: "api-port",
-      type: "number",
-      default: 3001,
-      describe: "port where logto will be listening",
-    },
-    db: {
-      alias: "database",
-      type: "string",
-      default: "dictycr_auth",
-      describe: "name of logto database",
-    },
-    ep: {
-      alias: "endpoint",
-      type: "string",
-      describe: "http endpoint for logto backend api service",
-      demandOption: true,
-    },
-  })
-  .help()
-  .completion()
-  .parseSync()
-
+const argv = process_logto_cmdline()
 const secret = await getSecret(argv.kc, argv.sr, argv.ns)
 const app = new App()
-const pvc = argv.nm.concat("-claim")
-new LogtoPersistentVolumeClaimStack(app, pvc, {
-  provider: {
-    config: argv.kc,
-    remote: argv.r,
-    credentials: argv.c,
-    bucketName: argv.bn,
-    bucketPrefix: pvc.concat(argv.ns),
-  },
-  resource: {
-    namespace: argv.ns,
-    diskSize: argv.ss,
-    storageClass: argv.sc,
-  },
-})
-const adminService = argv.nm.concat("-admin")
-const apiService = argv.nm.concat("-api")
-new LogtoBackendDeploymentStack(app, argv.nm, {
-  provider: {
-    config: argv.kc,
-    remote: argv.r,
-    credentials: argv.c,
-    bucketName: argv.bn,
-    bucketPrefix: argv.nm.concat(argv.ns),
-  },
-  resource: {
-    database: argv.db,
-    namespace: argv.ns,
-    image: argv.im,
-    tag: argv.tg,
-    secret: secret as V1Secret,
-    adminService: adminService,
-    apiService: apiService,
-    claim: pvc,
-    adminPort: argv.adp,
-    apiPort: argv.ap,
-    endpoint: argv.ep,
-  },
-})
-new BackendService(app, adminService, {
-  provider: {
-    config: argv.kc,
-    remote: argv.r,
-    credentials: argv.c,
-    bucketName: argv.bn,
-    bucketPrefix: adminService.concat("-").concat(argv.ns),
-  },
-  resource: {
-    namespace: argv.ns,
-    port: argv.adp,
-    app: argv.nm,
-  },
-})
-new BackendService(app, apiService, {
-  provider: {
-    config: argv.kc,
-    remote: argv.r,
-    credentials: argv.c,
-    bucketName: argv.bn,
-    bucketPrefix: apiService.concat("-").concat(argv.ns),
-  },
-  resource: {
-    namespace: argv.ns,
-    port: argv.adp,
-    app: argv.nm,
-  },
-})
+new LogtoPersistentVolumeClaimStack(app, pvc(argv), logtoPvcStackOptions(argv))
+new LogtoBackendDeploymentStack(
+  app,
+  argv.nm,
+  logtoBackendDeploymentOptions(argv, secret as V1Secret),
+)
+new BackendService(
+  app,
+  adminService(argv),
+  logtoServiceOptions(argv, adminService(argv)),
+)
+new BackendService(
+  app,
+  apiService(argv),
+  logtoServiceOptions(argv, apiService(argv)),
+)
 app.synth()
